d75714502bf22ec119c1379aee7fa97cc473950e
zyl <youluzheng@foxmail.com>
2021-06-30 21:13:07 +0800

    refactor:重构日志模块

--- logger.py
+++ logger.py
@@ -4,45 +4,62 @@ import os
 
 # 因为还没读取到配置文件，只能先用文件的方式打印日志到当前目录
 if not os.path.exists("config.ini"):
-    with open('output.log', 'a', encoding='utf-8') as f:
-        f.writelines("config.ini文件未找到!\n")
-        exit(1)
+    print("config.ini文件未找到，启动失败")
+    exit(1)
 
 cp = configparser.ConfigParser()
 cp.read("config.ini")
 
+is_logger_exists = True
+
 try:
     options = dict(cp.items("logger"))
 except configparser.NoSectionError:
-    with open('output.log', 'a', encoding='utf-8') as f:
-        f.writelines("[logger]配置项未找到!\n")
-        exit(1)
-
-
-path = options.get("path")
-file_name = options.get("file_name")
-level = options.get("level")
+    is_logger_exists = False
+    path = 'logs'
+    file_name = 'output.log'
+    level = 'INFO'
+else:
+    path = options.get("path")
+    file_name = options.get("file_name")
+    level = options.get("level")
 
 level_list = ['DEBUG', 'debug', 'INFO', 'info',
               'WARNING', 'warning', 'ERROR', 'error']
+
+if path is not None:
+    path_temp = path
+else:
+    path_temp = 'logs'
+
+# 如果不是当前目录
+if path_temp != '' and path != '.':
+    if not os.path.exists(path_temp):
+        os.makedirs(path_temp)
+
+file_name_temp = file_name if file_name else 'output.log'
+
+level_temp = level.upper() if level in level_list else 'INFO'
+
+if path_temp == '':
+    log_file_full_path = file_name_temp
+else:
+    log_file_full_path = path_temp + '/' + file_name_temp
 
 
 class Logger:
     def __init__(self, module: str):
         self.logger = logging.getLogger(module)
-        self.logger.setLevel(level.upper() if level in level_list else 'INFO')
+        self.logger.setLevel(level_temp)
         fmt = logging.Formatter(
-            '[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s', '%Y-%m-%d %H:%M:%S')
+            '[%(asctime)s] [%(levelname)s] %(message)s',
+            '%Y-%m-%d %H:%M:%S'
+        )
 
-        # 如果配置文件中没有path配置项
-        path_temp = path if path else ''
-        # 如果配置文件中没有file_name配置项
-        file_name_temp = file_name if file_name else 'output.log'
         # 设置文件日志
-        fh = logging.FileHandler(
-            file_name_temp if path_temp == '' else path_temp + '/' + file_name_temp)
+        fh = logging.FileHandler(log_file_full_path)
         fh.setFormatter(fmt)
-        fh.setLevel(level.upper() if level in level_list else 'INFO')
+        fh.setLevel(level_temp)
         self.logger.addHandler(fh)
 
     def debug(self, message):
@@ -58,31 +75,33 @@ class Logger:
         self.logger.error(message)
 
 
-__log = Logger("logger")
+log = Logger("logger")
 
-if path is None:
-    __log.warn("[logger]->path配置项未找到, 默认设置为当前目录")
+log.info("正在初始化日志配置...")
 
-# 如果设置为当前目录，特殊处理
-if path == '' or path == '.':
-    __log.info("设置日志目录:当前目录")
+if not is_logger_exists:
+    log.warn("[logger]配置项未找到, 使用默认配置初始化日志设置")
 
+if path != path_temp:
+    log.warn("[logger]->path配置项未找到, 默认设置为logs目录")
 else:
-    __log.info("设置日志目录:" + path)
-    if not os.path.exists(path):
-        __log.warn(path + "目录不存在, 正在创建中...")
-        os.makedirs(path)
-        __log.info(path + "目录创建完成!")
+    if path == '' or path == '.':
+        log.info("初始化日志目录:当前目录")
+    else:
+        log.info("初始化日志目录:" + path)
 
-if file_name is None:
-    __log.warn("[logger]->file_name配置项未找到，默认设置为output.log")
 
-__log.info("设置日志文件名:" + file_name)
+if file_name != file_name_temp:
+    log.warn("[logger]->file_name配置项未找到，默认设置为output.log")
+else:
+    log.info("初始化日志文件名称:" + file_name)
 
-if level is None:
-    __log.warn("[logger]->level配置项未找到，默认设置为INFO")
+if level.upper() != level_temp:
+    if level:
+        log.warn("level配置项无法识别" + level + "，默认设置为INFO")
+    else:
+        log.warn("[logger]->level配置项未找到，默认设置为INFO")
+else:
+    log.info("初始化日志级别:" + level_temp)
 
-if level not in level_list:
-    __log.warn("[logger]->level，" + level + "无法识别，默认设置为INFO")
-
-__log.info("设置日志级别:" + level)
+log.info("日志初始化配置完成!")
